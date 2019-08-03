f619dd5fff7d1b789311b423e1416a05f4b11b0f
Jack <supbscripter@gmail.com>
2019-08-03 11:51:18 +0100

    Basic AI Movement

---
+++ GMTK-Unity/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 velocity = target.transform.position - transform.position;
+        velocity.Normalize();
+        velocity /= 16;
+        transform.position += (Vector3)velocity;
+    }
+}
